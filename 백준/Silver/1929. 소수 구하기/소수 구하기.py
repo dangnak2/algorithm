0ca9a76d13eb3a842ff6fbf4de1cb657b6428c8b
김민기 <80161984+dangnak2@users.noreply.github.com>
2023-08-03 18:17:08 +0900

    Update 소수 구하기.py

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,35 +1,20 @@
-
-# 시간초과
-# n, m = list(map(int, input().split()))
-# a = list()
-# for i in range(n, m+1):
-#     cnt = 0
-#     if(i > 1):
-#         for j in range(2, i):
-#             if(i % j == 0):
-#                 cnt += 1
-#                 break
-#
-#     if(cnt != 1):
-#         a.append(i)
-#
-# for i in range(len(a)):
-#     print(a[i])
-
 n, m = list(map(int, input().split()))
 
 def isPrime(num):
+		# 1은 소수가 아님
     if(num==1):
         return False
 
+	# 2이상 부터 이중 for문으로 2부터 해당 수까지 반복문을 돌려 0으로 나누어떨어지는지 확인
+    # 나누어 떨어지면 소수가 아니므로 바로 False 
     else:
         for i in range(2, int(num**0.5) + 1):
             if(num%i == 0):
                 return False
+				# 이중 for문을 다 돌고 나오면, 0으로 나누어떨어지는 수가 없다는 의미
+				# 즉, 소수
         return True
 
 for i in range(n, m+1):
     if isPrime(i):
         print(i)
-
-
