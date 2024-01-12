a0cad5229fba9f3386b6daa1438997b73cd0fc78
김민기 <80161984+dangnak2@users.noreply.github.com>
2024-01-12 20:22:36 +0900

    [level Lv.1] Title: 시저 암호, Time: 94.43 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
+            // 공백처리
+            if(ch == ' '){
+                answer += ch;
+                continue;
+            }
+            
+            // 소문자
+            if(Character.isLowerCase(ch)){
+                if(ch + n > 'z'){
+                  answer += (char)(ch - 26 + n);  
+                } 
+                else {
+                    answer += (char)(ch + n);
+                }
+            }
+            // 대문자
+            else if(Character.isUpperCase(ch)){
+                if(ch + n > 'Z') {
+                    answer += (char)(ch - 26 + n);
+                }
+                else {
+                    answer += (char)(ch + n);
+                }
+            }
+        }
+        return answer;
+    }
+}
