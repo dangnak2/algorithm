ae1310832ca293fd5a5d5c0bea4a68e204c6f54a
김민기 <80161984+dangnak2@users.noreply.github.com>
2024-03-20 20:58:55 +0900

    [level 2] Title: 올바른 괄호, Time: 19.51 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,21 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
-        int open_count = 0;
-        int close_count = 0;
+        Stack<Integer> stack = new Stack<Integer>();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '(') open_count++;
-            else if(s.charAt(i) == ')') close_count++;
             
-            if(close_count > open_count) return false;
+            if(s.charAt(i) == '(') stack.push(1);
+            else if(s.charAt(i) == ')') {
+                if(stack.isEmpty()) return false;
+                
+                stack.pop();
+            }
+            
         }
-        if(open_count == close_count) return true;
-
+        if(stack.isEmpty()) return true;
+        
         return false;
     }
 }
